d8e6ff955c33001786d8bd6c1d53f68a8f9130a3
Erisoka <leonidas.asprilla@outlook.com>
2021-07-02 14:47:06 -0500

    Sesion 01 con Profesora Suplente

--- Practicas/Java Basic/src/clase01/ejemplo02.java
+++ Practicas/Java Basic/src/clase01/ejemplo02.java
@@ -2,12 +2,7 @@ package clase01;
 
 import java.util.Scanner;
 
-/*
-    Script en Java que dada una cantidad en bodega y una cantidad minima requerida indique si es necesario
-    o no solicitar el producto al proveer
-*/
-
-public class clase01 {
+public class ejemplo02 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int bodega, existencias_requeridas;
@@ -15,10 +10,13 @@ public class clase01 {
         bodega = entrada.nextInt();
         System.out.print("Ingrese la candtidad minima requerida: ");        
         existencias_requeridas = entrada.nextInt();
-        // System.out.print("Bodega: "+bodega+" - Existencias requeridas: "+existencias_requeridas);
         if (bodega < existencias_requeridas) {
             System.out.print("Debe realizar el pedido al proveedor.");
         } else {
+            int unidades_vener = bodega - existencias_requeridas;
+            if (unidades_vener < 10) {
+                System.out.print("No es necesario realizar el pedido.");
+            }
             System.out.print("No es necesario realizar el pedido al proveedor.");
         }
         entrada.close();
