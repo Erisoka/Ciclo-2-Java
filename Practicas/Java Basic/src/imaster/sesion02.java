003daf9b03d4d7bee5f8dee694420436497947d5
Erisoka <leonidas.asprilla@outlook.com>
2021-07-03 12:47:06 -0500

    Operadores aritemticos

---
+++ Practicas/Java Basic/src/imaster/sesion02.java
@@ -0,0 +1,23 @@
+package imaster;
+
+public class sesion02 {
+    /*
+    private String nombre;
+    private int identificacion;
+    private double estatura;
+    private float peso;
+    private char sexo;
+    private boolean estaVacunado;
+    */
+
+    public static void main(String[] args) {
+        // System.out.println("Hola mundo!");
+        int x = 5;
+        int y = 2;
+        int varEntero = x / y; 
+        float varFloat = (float)(x) / (float)(y); 
+        System.out.println(varEntero);
+        System.out.println(varFloat);
+
+    }
+}
