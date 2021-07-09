47d38f198d78a5a8409d5dc40772190d8fea2b73
Erisoka <leonidas.asprilla@outlook.com>
2021-07-09 14:03:56 -0500

    Ejemplo Sesion 03

--- Practicas/Java Basic/src/clase03/practica03.java
+++ Practicas/Java Basic/src/clase03/practica03.java
@@ -6,16 +6,25 @@ import java.util.Scanner;
 public class practica03 {
     public static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) {
-        int n, m, matriz[][];
+        int n, m, matA[][];
 
         System.out.print("Ingrese número de filas: ");
         m = leer.nextInt();
         System.out.print("Ingrese número de columnas: ");
         n = leer.nextInt();
-        matriz = new int[m][n];
-        llenarMatriz(matriz, m, n);
-        System.out.println("=== Datos de la matriz ===");
-        imprimirMatriz(matriz, m, n);
+        matA = new int[m][n];
+        llenarMatriz(matA, m, n);
+        System.out.println("=== Datos de la matriz A ===");
+        imprimirMatriz(matA, m, n);
+
+        int matB[][] = new int[m][n];
+        int matC[][] = new int[m][n];
+        llenarMatriz(matB, m, n);
+        System.out.println("=== Datos de la matriz B ===");
+        imprimirMatriz(matB, m, n);
+        matC = sumarMatrices(matA, matB, m, n);
+        System.out.println("=== Datos de la Matriz C Suma ===");
+        imprimirMatriz(matC, m, n);
 
         leer.close();
     }
@@ -40,5 +49,18 @@ public class practica03 {
             }
             System.out.println();
         }
+    }
+
+    // Método para sumar dos matrices
+    public static int[][] sumarMatrices(int mat1[][], int mat2[][], int m, int n) {
+        int resultado[][] = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                resultado[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+
+        return resultado;
     }
 }
